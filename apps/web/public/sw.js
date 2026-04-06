a733ac9c3ce37c9b8614c92805584b96878b611a
Omar Kebsi <Omar.-.2020@hotmail.com>
2026-04-06 14:01:37 +0300

    added rest of the icon images to sw

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,12 +1,20 @@
-const STATIC_CACHE = "seas-static-v11";
-const PAGE_CACHE = "seas-pages-v11";
+const STATIC_CACHE = "seas-static-v12";
+const PAGE_CACHE = "seas-pages-v12";
 const OFFLINE_URL = "/offline.html";
 
 const STATIC_ASSETS = [
   OFFLINE_URL,
   "/manifest.webmanifest",
   "/icon.ico",
+  "/icons/icon-48x48.png",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
   "/icons/icon-192x192.png",
+  "/icons/icon-256x256.png",
+  "/icons/icon-384x384.png",
   "/icons/icon-512x512.png",
 ];
 
